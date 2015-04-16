fc4fac265387dbb0784f8b2fa4735b2a317a509e
Jonathan Daggerhart <jonathan@daggerhart.com>
2015-04-15 23:32:40 -0400

    sing-a-long example

---
+++ includes/sing-a-long.php
@@ -0,0 +1,24 @@
+<?php
+
+class JokePack_SingAlong{
+
+
+	function __construct(){
+		add_action('jokepack_init', array( $this, 'init') );
+	}
+
+	function init(){
+		add_filter( 'the_content', array( $this, 'wrap_content' ), 9999 );
+		add_action('wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+	}
+
+	function enqueue_scripts(){
+		wp_enqueue_script('jokepack-sing-a-long', JOKEPACK_URL . '/js/sing-a-long.js', array('jquery') );
+	}
+
+	function wrap_content( $content ){
+		return "<div id='sing-a-long'>".$content."</div>";
+	}
+}
+
+new JokePack_SingAlong();
---
+++ js/sing-a-long.js
@@ -0,0 +1,12 @@
+(function($){
+
+  // a word is highlighted for some period of time
+  // a ball bounces above the word
+  // the ball bounces to the next word
+  $(document).ready(function(){
+    var words = $('#sing-a-long').text().split(' ');
+
+    console.log(words);
+  });
+
+})(jQuery);
