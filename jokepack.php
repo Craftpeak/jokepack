7f568c80dc327c7306e91b36e44ea138932e3e18
Jonathan Daggerhart <jonathan@daggerhart.com>
2015-04-15 22:10:51 -0400

    simply pluginable

--- jokepack.php
+++ jokepack.php
@@ -10,6 +10,14 @@ class JokePack {
 
 	function __construct(){
 		add_action('plugins_loaded', array( $this, 'jokepack_init' ) );
+
+		$base = dirname(__FILE__) .'/includes/';
+		$dir = new DirectoryIterator( $base );
+		foreach ( $dir as $file ) {
+			if ( !$file->isDot() ) {
+				include_once $base . $file->getFilename();
+			}
+		}
 	}
 
 	function jokepack_init(){
